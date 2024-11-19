7b45f640c0ae8587ec8a7542e90518dcc231f26b
Leon Boschman <leon.boschman@gmail.com>
2024-11-19 09:51:11 +0100

    Add batch script for exploration of parameter space.

---
+++ analysis/batchscript.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#SBATCH -A C3SE2024-1-22 -p vera
+#SBATCH -t 05:00:00
+#SBATCH -n 16
+
+module load virtualenv/20.23.1-GCCcore-13.3.0 PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1 h5py/3.9.0-foss-2023a
+module load scikit-learn/1.3.1-gfbf-2023a scikit-image/0.22.0-foss-2023a
+
+module list
+
+source training_venv/bin/activate
+
+echo "Starting the training process"
+
+cp -r ./* $TMPDIR/
+
+cd $TMPDIR
+
+input_file=$1
+
+eval `head -n $SLURM_ARRAY_TASK_ID $input_file | tail -1`
+echo "[`date`] Running learning rate = ${learning_rate} batch size=$batch_size filename=lr_${learning_rate}_bs_$batch_size"
+
+python3 train_2layer_model.py -c double_layer_config.toml -b $batch_size -l ${learning_rate} -n lr_${learning_rate}_bs_$batch_size
+
+cp logs/*.log $SLURM_SUBMIT_DIR/logs/
+cp *.pt $SLURM_SUBMIT_DIR/saved_models/
+cp -r runs/* $SLURM_SUBMIT_DIR/runs/
+
+echo "Finito"
+
